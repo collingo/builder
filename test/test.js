fa6bf0ff1d3863da141fc7e910a37207bd94f4ea
Nick Collings <nick@collingo.com>
2014-10-25 12:23:14 +0100

    initial commit

---
+++ test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var sandbox;
+
+//// SUT
+var Builder = require('../src/builder');
+
+describe('Builder', function() {
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('should write proper tests', function() {
+    expect(true).to.be.true;
+  });
+
+});
